41cdf91aaa4a8c3c53f29ae53e1c84ce2e09775c
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2018-12-07 11:13:09 +0100

    Updates

---
+++ precache-manifest.2269c0b3d4a0c467a4ca51994e9ff0cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52745c042de01f2a62b0",
+    "url": "/react-calculator/public/static/js/runtime~main.52745c04.js"
+  },
+  {
+    "revision": "f307737afbab53afc4d9",
+    "url": "/react-calculator/public/static/js/main.f307737a.chunk.js"
+  },
+  {
+    "revision": "4ed411020c525a9e3f1b",
+    "url": "/react-calculator/public/static/js/1.4ed41102.chunk.js"
+  },
+  {
+    "revision": "f307737afbab53afc4d9",
+    "url": "/react-calculator/public/static/css/main.fe3320db.chunk.css"
+  },
+  {
+    "revision": "7e5c99d96b70e2711e2dec4c6eddd735",
+    "url": "/react-calculator/public/index.html"
+  }
+];
