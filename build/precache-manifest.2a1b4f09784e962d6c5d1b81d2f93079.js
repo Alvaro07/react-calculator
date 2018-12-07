6a099a67f2147dde973e8ea7d7988e07915086e8
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2018-12-07 11:18:38 +0100

    Changes to deploy

---
+++ build/precache-manifest.2a1b4f09784e962d6c5d1b81d2f93079.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2ecf2e68de309a6bd2b",
+    "url": "/react-calculator/static/js/runtime~main.a2ecf2e6.js"
+  },
+  {
+    "revision": "f307737afbab53afc4d9",
+    "url": "/react-calculator/static/js/main.f307737a.chunk.js"
+  },
+  {
+    "revision": "4ed411020c525a9e3f1b",
+    "url": "/react-calculator/static/js/1.4ed41102.chunk.js"
+  },
+  {
+    "revision": "f307737afbab53afc4d9",
+    "url": "/react-calculator/static/css/main.fe3320db.chunk.css"
+  },
+  {
+    "revision": "a6332e05775e3135fdc88a31d8f046e3",
+    "url": "/react-calculator/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-calculator/build/precache-manifest.275869141093e13e9a507332230725ee.js"
+  "/react-calculator/precache-manifest.2a1b4f09784e962d6c5d1b81d2f93079.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-calculator/build/index.html", {
+workbox.routing.registerNavigationRoute("/react-calculator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
