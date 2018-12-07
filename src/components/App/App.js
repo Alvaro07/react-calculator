87d2fa4455be5fdd43e1c012641ae04ff9651d71
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2018-12-07 10:47:53 +0100

    First commit

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Calculator from '../Calculator/Calculator';
+
+// app.js
+// App component
+
+const App = () => {
+	return(
+		<div className="app-wrap">
+			<Calculator />
+		</div>	
+	)
+}
+
+export default App;
